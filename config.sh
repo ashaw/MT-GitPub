d22eb09a37e1cb556394fce023f17d92bdbbdec1
Al Shaw <almshaw@gmail.com>
2009-09-21 22:54:36 -0400

    publishing index templates

--- config.sh
+++ config.sh
@@ -6,16 +6,16 @@ GITDIR=/var/www/mt-shhhaw-theme-working
 # define blog id
 # blog_id="1"
 
-blog_id="1"
+blog_id="3"
 
 # define index templates and archive templates like so
 # (leave comment hashes)
 #
-# # indextmpl filename mt template title
+# #indextmpl filename mt template title
 #
 
 #indextmpl index.mtml index
-#indextmpl style.css styles
+#indextmpl style.css style
 #indextmpl atom.mtml atom
 #indextmpl the-archive.mtml full archive
-#indextmpl module-head.mtml header module
+#indextmpl module-head.mtml head
